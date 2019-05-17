1807eb815f5e823b94da456526264a56e4c4509f
zhangyicheng1986@outlook.com <zhangyicheng1986@outlook.com>
2019-05-17 10:38:47 +0800

    test push branch

--- model.py
+++ model.py
@@ -63,67 +63,43 @@ class Model(object):
         n = self.num_class
 
         # build loss and accuracy {{{
-        def build_loss(d_real, d_real_logits, d_fake, d_fake_logits, label, real_image, fake_image):
+        def build_loss(d_real, d_real_logits, label):
             alpha = 0.9
-            real_label = tf.concat([label, tf.zeros([self.batch_size, 1])], axis=1)
-            fake_label = tf.concat([(1-alpha)*tf.ones([self.batch_size, n])/n,
-                                    alpha*tf.ones([self.batch_size, 1])], axis=1)
 
             # Discriminator/classifier loss
-            s_loss = tf.reduce_mean(huber_loss(label, d_real[:, :-1]))
+            s_loss = tf.reduce_mean(huber_loss(label, d_real))
             d_loss_real = tf.nn.softmax_cross_entropy_with_logits(
-                logits=d_real_logits, labels=real_label)
-            d_loss_fake = tf.nn.softmax_cross_entropy_with_logits(
-                logits=d_fake_logits, labels=fake_label)
-            d_loss = tf.reduce_mean(d_loss_real + d_loss_fake)
+                logits=d_real_logits, labels=label)
 
-            # Generator loss
-            g_loss = tf.reduce_mean(tf.log(d_fake[:, -1]))
+            d_loss = tf.reduce_mean(d_loss_real)
 
-            # Weight annealing
-            g_loss += tf.reduce_mean(
-                huber_loss(real_image, fake_image)) * self.recon_weight
 
-            GAN_loss = tf.reduce_mean(d_loss + g_loss)
 
             # Classification accuracy
-            correct_prediction = tf.equal(tf.argmax(d_real[:, :-1], 1),
+            correct_prediction = tf.equal(tf.argmax(d_real, 1),
                                           tf.argmax(self.label, 1))
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            return s_loss, d_loss_real, d_loss_fake, d_loss, g_loss, GAN_loss, accuracy
+            return s_loss, d_loss_real, d_loss, accuracy
         # }}}
 
-        # Generator {{{
-        # =========
-        G = Generator('Generator', self.h, self.w, self.c,
-                      self.norm_type, self.deconv_type, is_train)
 
-        z = tf.random_normal(shape=[self.batch_size, self.n_z], mean=0, stddev=1, dtype=tf.float32)
-        # z = tf.random_uniform([self.batch_size, self.n_z],
-        #                       minval=-1, maxval=1, dtype=tf.float32)
-        fake_image = G(z)
-        self.fake_image = fake_image
-        # }}}
 
         # Discriminator {{{
         # =========
         D = Discriminator('Discriminator', self.num_class, self.norm_type, is_train,self.h, self.w, self.c)
         d_real, d_real_logits = D(self.image)
-        d_fake, d_fake_logits = D(fake_image)
+
         self.all_preds = d_real
         self.all_targets = self.label
         # }}}
 
-        self.S_loss, d_loss_real, d_loss_fake, self.d_loss, self.g_loss, GAN_loss, self.accuracy = \
-            build_loss(d_real, d_real_logits, d_fake, d_fake_logits, self.label, self.image, fake_image)
+        self.S_loss, d_loss_real, self.d_loss,  self.accuracy = \
+            build_loss(d_real, d_real_logits, self.label)
 
         tf.summary.scalar("loss/accuracy", self.accuracy)
-        tf.summary.scalar("loss/GAN_loss", GAN_loss)
         tf.summary.scalar("loss/S_loss", self.S_loss)
         tf.summary.scalar("loss/d_loss", tf.reduce_mean(self.d_loss))
         tf.summary.scalar("loss/d_loss_real", tf.reduce_mean(d_loss_real))
-        tf.summary.scalar("loss/d_loss_fake", tf.reduce_mean(d_loss_fake))
-        tf.summary.scalar("loss/g_loss", tf.reduce_mean(self.g_loss))
         # tf.summary.image("img/fake", fake_image)
         # tf.summary.image("img/real", self.image, max_outputs=1)
         # tf.summary.image("label/target_real", tf.reshape(self.label, [1, self.batch_size, n, 1]))
