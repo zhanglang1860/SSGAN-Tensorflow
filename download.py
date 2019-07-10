bdaa4d01576eb016fccc92396c6821f510f9c96b
ruoshuima <zhangyicheng1986@outlook.com>
2019-07-10 19:28:20 +0800

    80   10   10   split train test validation

--- download.py
+++ download.py
@@ -7,8 +7,7 @@ import h5py
 import numpy as np
 import glob
 from random import shuffle
-import tables
-import cv2
+
 from math import ceil
 import matplotlib.pyplot as plt
 
@@ -71,8 +70,8 @@ def prepare_h5py_mri2_8020(image, label, data_dir, num_class=10,shape=None,first
     data_id = open(os.path.join(data_dir, id_file_name), 'w')
     for i in range(image.shape[0]):
 
-        if i % (image.shape[0] / 100) == 0:
-            bar.update(i / (image.shape[0] / 100))
+        # if i % (image.shape[0] / 100) == 0:
+        #     bar.update(i / (image.shape[0] / 100))
 
         grp = f.create_group(str(i))
         data_id.write(str(i) + '\n')
@@ -171,56 +170,6 @@ def check_file(data_dir):
     return False
 
 
-def download_mnist(download_path):
-    data_dir = os.path.join(download_path, 'mnist')
-
-    if check_file(data_dir):
-        print('MNIST was downloaded.')
-        return
-
-    data_url = 'http://yann.lecun.com/exdb/mnist/'
-    keys = ['train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz',
-            't10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz']
-    # keys = [ 'train-labels-idx1-ubyte.gz']
-
-    # for k in keys:
-    #     url = (data_url+k).format(**locals())
-    #     target_path = os.path.join(data_dir, k)
-    #     print(target_path)
-    #     cmd = ['curl', url, '-o', target_path]
-    #     print('Downloading ', k)
-    #     subprocess.call(cmd)
-    #     # cmd = ['gzip', '-d', target_path]
-    #     cmd = ['7z', 'e', target_path]
-    #     print('Unzip ', k)
-    #     # subprocess.call(cmd)
-    # print('OK ', k)
-    num_mnist_train = 60000
-    num_mnist_test = 10000
-
-    fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    train_image = loaded[16:].reshape((num_mnist_train, 28, 28, 1)).astype(np.float)
-
-    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    train_label = np.asarray(loaded[8:].reshape((num_mnist_train)).astype(np.float))
-
-    fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    test_image = loaded[16:].reshape((num_mnist_test, 28, 28, 1)).astype(np.float)
-
-    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    test_label = np.asarray(loaded[8:].reshape((num_mnist_test)).astype(np.float))
-
-    prepare_h5py(train_image, train_label, test_image, test_label, data_dir)
-
-    # for k in keys:
-    #     cmd = ['rm', '-f', os.path.join(data_dir, k[:-3])]
-    #     subprocess.call(cmd)
-
-
 
 def convert_to_one_hot(label, num_classes,batch_size):
   """Convert class labels from scalars to one-hot vectors."""
@@ -231,71 +180,6 @@ def convert_to_one_hot(label, num_classes,batch_size):
   # labels_one_hot[label]=1
 
   return labels_one_hot
-
-
-def download_ulna(download_path):
-    shuffle_data = True
-    data_dir = os.path.join(download_path, 'ulna')
-    hdf5_path = os.path.join(data_dir, '/data.hdf5')
-    all_jpg_path=os.path.join(data_dir, 'all')
-    all_jpg_path = os.path.join(all_jpg_path, '/*.jpg')
-    all_jpg_path = '/media/wenyu/8d268d3e-37df-4af4-ab98-f5660b2e71a7/wenyu/PycharmProjects/SSGAN-original-Tensorflow/datasets/ulna/all/*.jpg'
-
-    if check_file(data_dir):
-        print('ulna was downloaded.')
-        return
-
-    # read addresses and labels from the 'train' folder
-    addrs = glob.glob(all_jpg_path)
-    labels=[]
-    for addr in addrs:
-        image_name = addr.split('/')
-        temp = image_name[10].split('_')
-        labels.append(temp[0])
-
-    if shuffle_data:
-        c = list(zip(addrs, labels))
-        shuffle(c)
-        addrs, labels = zip(*c)
-
-    # Divide the hata into 60% train, 20% validation, and 20% test
-    train_addrs = addrs[0:int(0.6 * len(addrs))]
-    train_labels = labels[0:int(0.6 * len(labels))]
-
-    # val_addrs = addrs[int(0.6 * len(addrs)):int(0.8 * len(addrs))]
-    # val_labels = labels[int(0.6 * len(addrs)):int(0.8 * len(addrs))]
-
-    test_addrs = addrs[int(0.6 * len(addrs)):]
-    test_labels = labels[int(0.6 * len(labels)):]
-    train_label = np.array(train_labels, dtype=np.float)
-    test_label = np.array(test_labels, dtype=np.float)
-
-    train_images =[]
-    test_images = []
-
-    for i in range(len(train_addrs)):
-        addr = train_addrs[i]
-        img = cv2.imread(addr)
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray_img = gray_img[..., None]
-        train_images.append(gray_img)
-
-    for i in range(len(test_addrs)):
-        addr = test_addrs[i]
-        img = cv2.imread(addr)
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray_img = gray_img[..., None]
-        test_images.append(gray_img)
-
-
-    train_image = np.array(train_images, dtype=np.float)
-    test_image = np.array(test_images, dtype=np.float)
-
-
-
-
-
-    prepare_h5py(train_image, train_label, test_image, test_label, data_dir,3)
 
 
 
@@ -383,7 +267,7 @@ def download_mri23_class8020(download_path):
 
     data_dir = os.path.join(download_path, 'mri')
 
-    all_jpg_path = '/data1/wenyu/PycharmProjects/SSGAN-original-Tensorflow/datasets/mri/all/'
+    all_jpg_path = './datasets/mri/all/'
 
     if check_file(data_dir):
         print('mri was downloaded.')
@@ -402,7 +286,7 @@ def download_mri23_class8020(download_path):
     #     image_class_list[i] = []
     #     label_class_list[i] = []
 
-    with open("/data1/wenyu/PycharmProjects/SSGAN-original-Tensorflow/datasets/mri/label.csv") as file:
+    with open("./datasets/mri/label.csv") as file:
         for line in file:
             strFull = line.split(',')
             strFull[0]=strFull[0]+'.nii'
@@ -421,8 +305,10 @@ def download_mri23_class8020(download_path):
 
     all_images_train = []
     all_images_test = []
+    all_images_validate = []
     all_labels_train = []
     all_labels_test = []
+    all_labels_validate = []
 
     for i in range(len(real_labels)):
         addrs = image_class_list[i]
@@ -437,7 +323,10 @@ def download_mri23_class8020(download_path):
             mri = np.array(mri) / mx
             data[:, :, :, 0] = mri
             mri_final = np.array(data)
-            if index<55:
+            if index<27:
+                all_images_validate.append(mri_final)
+                all_labels_validate.append(labels[index])
+            elif index>26 and index<54:
                 all_images_test.append(mri_final)
                 all_labels_test.append(labels[index])
             else:
@@ -446,13 +335,16 @@ def download_mri23_class8020(download_path):
 
     all_labels_train = np.array(all_labels_train, dtype=np.uint8)
     all_labels_test = np.array(all_labels_test, dtype=np.uint8)
+    all_labels_validate = np.array(all_labels_validate, dtype=np.uint8)
 
 
+    all_images_validate = np.array(all_images_validate)
     all_images_test = np.array(all_images_test)
     all_images_train = np.array(all_images_train)
 
     prepare_h5py_mri3(all_images_train, all_labels_train, data_dir, "train_", 3)
     prepare_h5py_mri3(all_images_test, all_labels_test, data_dir, "test_", 3)
+    prepare_h5py_mri3(all_images_validate, all_labels_validate, data_dir, "validate_", 3)
 
 
 
@@ -462,8 +354,10 @@ def download_mri23_class8020(download_path):
 
             all_images_train = []
             all_images_test = []
+            all_images_validate = []
             all_labels_train = []
             all_labels_test = []
+            all_labels_validate = []
 
 
             addrs=image_class_list[i]+image_class_list[j]
@@ -491,12 +385,18 @@ def download_mri23_class8020(download_path):
                 data[:, :, :, 0] = mri
                 mri_final = np.array(data)
 
-                if index < int(len(addrs)*0.1+1):
+                if index < int(len(addrs)*0.05):
                     all_images_test.append(mri_final)
                     all_labels_test.append(labels[index])
-                elif (index > class_0_count and index < class_0_count +int(len(addrs)*0.1)+1):
+                elif (index > int(len(addrs)*0.05) and index < 2*int(len(addrs)*0.05)+1):
+                    all_images_validate.append(mri_final)
+                    all_labels_validate.append(labels[index])
+                elif (index > class_0_count and index < class_0_count +int(len(addrs)*0.05)):
                     all_images_test.append(mri_final)
                     all_labels_test.append(labels[index])
+                elif (index > class_0_count +int(len(addrs)*0.05)) and (index < class_0_count + 2*(int(len(addrs) * 0.05))):
+                    all_images_validate.append(mri_final)
+                    all_labels_validate.append(labels[index])
                 else:
                     all_images_train.append(mri_final)
                     all_labels_train.append(labels[index])
@@ -508,11 +408,16 @@ def download_mri23_class8020(download_path):
 
             all_labels_train = np.array(all_labels_train, dtype=np.uint8)
             all_images_train = np.array(all_images_train)
+            all_images_validate = np.array(all_images_validate)
+            all_labels_validate = np.array(all_labels_validate, dtype=np.uint8)
+
             all_labels_test = np.array(all_labels_test, dtype=np.uint8)
             all_images_test = np.array(all_images_test)
 
             prepare_h5py_mri2_8020(all_images_train, all_labels_train, data_dir, 2,first_class_label=real_labels[i],second_class_label=real_labels[j],train_test_name='train_')
             prepare_h5py_mri2_8020(all_images_test, all_labels_test, data_dir, 2, first_class_label=real_labels[i],second_class_label=real_labels[j], train_test_name='test_')
+            prepare_h5py_mri2_8020(all_images_validate, all_labels_validate, data_dir, 2, first_class_label=real_labels[i], second_class_label=real_labels[j], train_test_name='validate_')
+
             j=j+1
 
 
@@ -580,89 +485,6 @@ def show_img2(ori_img):
     plt.show()
 
 
-def download_svhn(download_path):
-    data_dir = os.path.join(download_path, 'svhn')
-
-    import scipy.io as sio
-    # svhn file loader
-    def svhn_loader(url, path):
-        cmd = ['curl', url, '-o', path]
-        subprocess.call(cmd)
-        m = sio.loadmat(path)
-        return m['X'], m['y']
-
-    if check_file(data_dir):
-        print('SVHN was downloaded.')
-        return
-
-    data_url = 'http://ufldl.stanford.edu/housenumbers/train_32x32.mat'
-    train_image, train_label = svhn_loader(data_url, os.path.join(data_dir, 'train_32x32.mat'))
-
-    data_url = 'http://ufldl.stanford.edu/housenumbers/test_32x32.mat'
-    test_image, test_label = svhn_loader(data_url, os.path.join(data_dir, 'test_32x32.mat'))
-
-    prepare_h5py(np.transpose(train_image, (3, 0, 1, 2)), train_label,
-                 np.transpose(test_image, (3, 0, 1, 2)), test_label, data_dir)
-
-    # cmd = ['rm', '-f', os.path.join(data_dir, '*.mat')]
-    # subprocess.call(cmd)
-
-
-def download_cifar10(download_path):
-    data_dir = os.path.join(download_path, 'cifar10')
-
-    # cifar file loader
-    def unpickle(file):
-        import cPickle
-        with open(file, 'rb') as fo:
-            dict = cPickle.load(fo)#dict = cPickle.load(fo, encoding='bytes')
-        return dict
-
-    if check_file(data_dir):
-        print('CIFAR was downloaded.')
-        return
-
-    data_url = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-    k = 'cifar-10-python.tar.gz'
-    target_path = os.path.join(data_dir, k)
-    print(target_path)
-    #cmd = ['curl', data_url, '-o', target_path]
-    print('Downloading CIFAR10')
-    #subprocess.call(cmd)
-    tarfile.open(target_path, 'r:gz').extractall(data_dir)
-
-    num_cifar_train = 50000
-    num_cifar_test = 10000
-
-    target_path = os.path.join(data_dir, 'cifar-10-batches-py')
-    train_image = []
-    train_label = []
-    for i in range(5):
-        fd = os.path.join(target_path, 'data_batch_' + str(i + 1))
-        dict = unpickle(fd)
-
-        sys.stdout = Logger(r'/media/wenyu/8d268d3e-37df-4af4-ab98-f5660b2e71a7/wenyu/PycharmProjects/SSGAN-original-Tensorflow/a.txt')
-        print(dict)
-        print('------------------')
-
-        train_image.append(dict[b'data'])
-        train_label.append(dict[b'labels'])
-
-
-    train_image = np.reshape(np.stack(train_image, axis=0), [num_cifar_train, 32 * 32 * 3])
-    train_label = np.reshape(np.array(np.stack(train_label, axis=0)), [num_cifar_train])
-
-    fd = os.path.join(target_path, 'test_batch')
-    dict = unpickle(fd)
-    test_image = np.reshape(dict[b'data'], [num_cifar_test, 32 * 32 * 3])
-    test_label = np.reshape(dict[b'labels'], [num_cifar_test])
-
-    prepare_h5py(train_image, train_label, test_image, test_label, data_dir, [32, 32, 3])
-
-    cmd = ['rm', '-f', os.path.join(data_dir, 'cifar-10-python.tar.gz')]
-    subprocess.call(cmd)
-    cmd = ['rm', '-rf', os.path.join(data_dir, 'cifar-10-batches-py')]
-    subprocess.call(cmd)
 
 
 import sys
@@ -686,7 +508,7 @@ class Logger(object):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    path = r"/data1/wenyu/PycharmProjects/SSGAN-original-Tensorflow/datasets/mri/all"
+    path = r"./datasets/mri/all"
 
 
     if not os.path.exists(path): os.mkdir(path)
